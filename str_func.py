e90b44a62a71b9522eecb1de6b3163eb24c91a91
Kruck <reka_931@mail.ru>
2023-04-03 20:37:48 +0300

    added a function to capitalize every letter in a word

--- str_func.py
+++ str_func.py
@@ -1,3 +1,10 @@
 def uppercase_string(word):
+    """получает строку и делает все буквы заглавными"""
     uppercase_word = word.upper()
     return uppercase_word
+
+
+def title_sentence(string):
+    """получает строку и делает первые буквы слов заглавными"""
+    title_words = ' '.join(word[0].upper() + word[1:] for word in string.split())
+    return title_words
